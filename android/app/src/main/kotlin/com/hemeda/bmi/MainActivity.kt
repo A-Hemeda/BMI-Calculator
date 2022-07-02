f2ff2b55ea354963b88e80bd010e8ec861f1d7ab
Ahmed Hemeda <101954795+A-Hemeda@users.noreply.github.com>
2022-07-02 22:02:33 +0200

    Add files via upload

---
+++ android/app/src/main/kotlin/com/hemeda/bmi/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.hemeda.bmi
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ android/app/src/main/kotlin/com/hemeda/bmi_calculator/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.hemeda.bmi_calculator
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
