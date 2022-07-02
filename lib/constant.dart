400836707abb59ad733d9d0ff2db58274f5ba68d
Ahmed Hemeda <101954795+A-Hemeda@users.noreply.github.com>
2022-07-02 22:03:19 +0200

    Add files via upload

---
+++ lib/constant.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+const kBottomContainerHeight = 60.0;
+const kActiveCardColor = Color(0xFF002040);
+const kInactiveCardColor = Color(0xFF001020);
+const kBottomContainerColor = Color(0xFF500500);
+
+const kLabelTextStyle = TextStyle(
+  fontSize: 20.0,
+  color: Color(0xFFAAAAAA),
+);
+
+const kNumberStyle = TextStyle(
+  fontSize: 50.0,
+  fontWeight: FontWeight.w900,
+);
+
+const kLargeButtonTextStyle = TextStyle(
+  fontSize: 25.0,
+  fontWeight: FontWeight.bold,
+);
+
+const kTitleTextStyle = TextStyle(
+  fontSize: 50.0,
+  fontWeight: FontWeight.bold,
+);
+
+const kResultTextStyle = TextStyle(
+    color: Color(0xFFCCCCCC), fontSize: 27.0, fontWeight: FontWeight.bold);
+
+const kBMIStyle = TextStyle(
+  fontSize: 100.0,
+  fontWeight: FontWeight.bold,
+);
+
+const kBodyStyle = TextStyle(
+  fontSize: 22.0,
+);
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'input_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark().copyWith(
+        primaryColor: Color(0xFF002040),
+        scaffoldBackgroundColor: Color(0xFF001020),
+      ),
+      home: InputPage(),
+    );
+  }
+}
